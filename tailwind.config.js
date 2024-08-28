f63889e39f3ade94f2ae90b2a4d908247b35c1c2
Johora99 <fatimatuzjohora1999@gmail.com>
2024-08-29 01:45:33 +0600

    card section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         'gray-1': 'rgba(102, 102, 102, 1)',
         'gray-2':'rgba(153, 153, 153, 1)',
         'gray-3':'rgba(77, 77, 77, 1)',
+        'gray-4':'rgba(242, 242, 242, 1)',
         'border-1':'rgba(128, 128, 128, 1)',
         'border-2':'rgba(230, 230, 230, 1)',
         'black-1':'rgba(0, 38, 3, 1)',
